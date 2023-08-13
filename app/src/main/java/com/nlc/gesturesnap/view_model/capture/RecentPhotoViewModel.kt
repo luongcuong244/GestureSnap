c6e183901dcf969a2a81ee4a428e4a21a54b5912
cuong_nl <luongcuong244@gmail.com>
2023-08-14 01:09:23 +0700

    fix bug: recent photo isn't update when the last photo was deleted

--- app/src/main/java/com/nlc/gesturesnap/view_model/capture/RecentPhotoViewModel.kt
+++ app/src/main/java/com/nlc/gesturesnap/view_model/capture/RecentPhotoViewModel.kt
@@ -1,15 +1,32 @@
 package com.nlc.gesturesnap.view_model.capture
 
+import android.content.Context
 import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.nlc.gesturesnap.R
+import com.nlc.gesturesnap.helper.MediaHelper
 
 class RecentPhotoViewModel : ViewModel() {
     private val _recentPhoto = MutableLiveData<Bitmap>()
 
     val recentPhoto : LiveData<Bitmap> = _recentPhoto
 
+    fun updateRecentPhoto(context: Context){
+        val photoPath = MediaHelper.getLatestPhotoPath(context)
+
+        val bitmap = if(photoPath != null){
+            BitmapFactory.decodeFile(photoPath)
+        } else {
+            BitmapFactory.decodeResource(context.resources, R.drawable.gallery_frame)
+        }
+
+        bitmap?.let {
+            setRecentPhoto(bitmap)
+        }
+    }
     fun setRecentPhoto(bitmap: Bitmap){
         _recentPhoto.value = bitmap
     }
